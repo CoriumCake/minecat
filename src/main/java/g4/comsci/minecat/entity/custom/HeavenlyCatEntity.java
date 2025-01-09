889319020d5602420429373d0319924f53fd2949
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2025-01-10 01:13:07 +0700

    make heaven cat tamable

--- src/main/java/g4/comsci/minecat/entity/custom/HeavenlyCatEntity.java
+++ src/main/java/g4/comsci/minecat/entity/custom/HeavenlyCatEntity.java
@@ -8,29 +8,37 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class HeavenlyCatEntity extends AnimalEntity {
+import static net.minecraft.util.JsonHelper.getItem;
 
-    public HeavenlyCatEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+public class HeavenlyCatEntity extends TameableEntity {
+
+    public HeavenlyCatEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new SitGoal(this));
         this.goalSelector.add(1, new AnimalMateGoal(this, 1.15));
         this.goalSelector.add(2, new TemptGoal(this, 1.25D, Ingredient.ofItems(ModItems.PURRIUM, ModItems.CAT_TEASER, ModItems.CATFOOD), false));
         this.goalSelector.add(3, new FollowParentGoal(this, 1.15D));
+        this.goalSelector.add(3, new FollowOwnerGoal(this, 1.15D, 10f, 3f, false));
         this.goalSelector.add(4, new WanderAroundFarGoal(this, 1D));
         this.goalSelector.add(5, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
         this.goalSelector.add(6, new LookAroundGoal(this));
@@ -68,5 +76,49 @@ public class HeavenlyCatEntity extends AnimalEntity {
     @Override
     protected @Nullable SoundEvent getDeathSound() {
         return SoundEvents.ENTITY_CAT_DEATH;
+    }
+
+    /* Tamable */
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getStackInHand(hand);
+        Item item = itemstack.getItem();
+
+        Item itemForTaming = ModItems.CATFOOD;
+
+        if(item == itemForTaming && !isTamed()){
+            if(this.getWorld().isClient()){
+                return ActionResult.CONSUME;
+            } else {
+                if (!player.getAbilities().creativeMode) {
+                    itemstack.decrement(1);
+                }
+
+                super.setOwner(player);
+                this.navigation.recalculatePath();
+                this.setTarget(null);
+                this.getWorld().sendEntityStatus(this, (byte)7);
+                setSitting(true);
+                setInSittingPose(true);
+
+                return ActionResult.SUCCESS;
+            }
+        }
+
+        if(isTamed() && hand == Hand.MAIN_HAND){
+            boolean sitting = !isSitting();
+            setSitting(sitting);
+            setInSittingPose(sitting);
+
+            return ActionResult.SUCCESS;
+        }
+
+        return super.interactMob(player, hand);
+    }
+
+    @Override
+    public EntityView method_48926() {
+        return this.getWorld();
     }
 }
